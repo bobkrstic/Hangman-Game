67db7f7ea88127a1f6092bfae63b55f261d6dc00
Slobodan Krstic <slobodankrstic@Slobodans-MacBook-Pro.local>
2017-05-07 21:33:44 -0700

    Some more work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,17 +24,25 @@
 // wins + 1
 // start game again.
 
-var wordBank = ["scocpe", "gloobe", "parernt", "crarpet", "moumse"];
-var chosenLetters = [];
-var linesWord = [];
-var wins = 0;
-var lives = 10;
-var matchFound;
+
+
+
+function startGame() 
+{
+	var wordBank = ["scope", "globe", "parents", "carpet", "mouse", "america", "thailand", "snakes", "shelves", "cooling", "museum"];
+	var chosenLetters = [];
+	var linesWord = [];
+	var wins = 0;
+	var lives = 10;
+	var matchFound;
+	// var a;
+	// var b;
+	
 
     // computer choses random word out of our array of words
 	// our array of different words is wordBank[]
 	var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-     // randWord.split('');
+	// a = randWord.toString();
 	console.log(randWord);
 
 		// when the word is chosen, count the number of letters in it
@@ -44,81 +52,100 @@ var matchFound;
     	   linesWord[j] = "_";
 		}
 
-	
-
-document.onkeyup = function(event) 
-{
-
-	var userGuess = event.key;
-	//var matchFound = 0;
-	//matchFound = 0;
-	console.log(event.key);
-    document.querySelector("#wordToGuess").innerHTML = randWord;
-
-
+	//display number of lines/number of letters in the word
 	document.querySelector("#linesWORD").innerHTML = linesWord;
-	
-	// document.querySelector("#chosenLetters").innerHTML = chosenLetters;
-	console.log(linesWord);
-	
-		
 
-	// iterate through the array that the computer randomly chose
-	// and compare to check if the letter chosen by user is in the word. 
-	
-		
-		matchFound = 0; 
 
-		for (var i = 0; i < randWord.length; i++) {
+// while (a !== b) {
+	//document.onkeyup = function(event) 
+	function myGame()
+	{
+		// get user's input ands store it in the userGuess variable
+		var userGuess = event.key;
 
-			console.log(randWord[i]);
-			if (userGuess == randWord[i]) {
-				console.log("There is a letter like that");
-				linesWord[i] = userGuess;
-				console.log(linesWord);
-				matchFound = matchFound + 1;
-				console.log("randWord matches: " + matchFound);	
-			}
-	    }
+		// display the random word
+	    document.querySelector("#wordToGuess").innerHTML = randWord;
+			
 
-		
-		//matchFound = 0; 
+		// iterate through the array that the computer randomly chose
+		// and compare to check if the letter chosen by user is in the word. 
+			matchFound = 0; 
+
+			for (var i = 0; i < randWord.length; i++) {
+
+				if (userGuess == randWord[i]) {
+					linesWord[i] = userGuess;
+					matchFound = matchFound + 1;
+				}
+		    }
+
+			// check the array of chosen letters if user already chose
+			// the letter. Set match found to 1
 			for (var s = 0; s < chosenLetters.length; s++) {
-				//matchFound = 0; 
 				if (userGuess == chosenLetters[s]) {
-				matchFound = matchFound + 1;
-				console.log("chosenLetters matched: " + matchFound);
+					matchFound = matchFound + 1;
 				}
 			}
 
+
+			
+
+			// add the chosen letter to the chosenLetters array
 			chosenLetters.push(userGuess);
 
-
+			// if no match letter found, deduct lives by one.
 			if (matchFound == 0) {
 				lives = lives - 1;	
+			} 
+
+
+			// a = [randWord].toString();
+			// b = [linesWord].toString();
+
+			// console.log("This is randWord " + a);
+			// console.log("This is linesWord " + b);
+
+
+			if (randWord === linesWord) {
+				wins = wins + 1;
+				alert("You won");
+				alert("Press any key to start again.");
+				// console.log(a===b);
 			}
 
 
+			// present to the user the chosenLetters array
+			document.querySelector("#chosenLetters").innerHTML = chosenLetters;
 
-		//chosenLetters.push(userGuess);
-		document.querySelector("#chosenLetters").innerHTML = chosenLetters;
-		console.log("Here goes chosenLetters: " + chosenLetters);
-		console.log("HEre lives: " + lives);
+			// update guessed letters to the user to see what is left to guess
+			document.querySelector("#linesWORD").innerHTML = linesWord;
 
-		document.querySelector("#linesWORD").innerHTML = linesWord;
+			// display remaining lives
+			document.querySelector("#livesRemaining").innerHTML = lives;
 
-		document.querySelector("#livesRemaining").innerHTML = lives;
-		
-}
+			// display number of wins
+			document.querySelector("#numberOfWIns").innerHTML = wins;
+
+			if (lives == 0) {
+				alert("You lost");
+				alert("Press any key to start again.");
+				//location.reload();
+				startGame();
+			}
+			
+			
+	};
+
+
+	document.onkeyup = myGame;
+
+// 	b = linesWord.toString();
+// }
+};
 
 
 
-
-
-
-
-
-
+document.onkeyup = startGame;
 
 
 
